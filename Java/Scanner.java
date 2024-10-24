ffac96735780a45f39b23b95a428840e0c444dfa
Niklas Lundberg <niklas@firedev.se>
2024-10-24 21:10:18 +0200

    small updates

---
+++ Java/Scanner.java
@@ -0,0 +1,8 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.craftinginterpreters.lox.TokenType.*;
